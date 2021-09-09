6d1af04f52b2b41b4783acab15740d3320d835f4
jamesjh1984 <24099235@qq.com>
2021-09-09 23:46:14 +0800

    springboot+mybatis+swagger2+HikariCP+junit test+ehcache+Quartz+Redis
    springboot
    mybatis(操作数据库)，call Mysql stored procedure
    swagger2(api 生成器)
    HikariCP(数据源 连接池)
    junit test
    ehcache(Cache缓存)
    Quartz(定时Job执行)
    @Transactional(事务控制)
    spring-boot-starter-validation(数据校验)
    Redis(Nosql, 内存数据库)

--- src/main/java/com/jin/springboot/controller/RedisController.java
+++ src/main/java/com/jin/springboot/controller/RedisController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+// http://localhost:8080/redis
 @RestController
 @RequestMapping("/redis")
 public class RedisController {
